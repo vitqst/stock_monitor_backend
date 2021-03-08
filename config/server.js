43955195372298ef44656cda03794ed438e28644
vuidx <vitqst@gmail.com>
2021-03-08 11:56:33 +0700

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4856f8db12966043b4512257d6fb11f8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '126d92bd-136c-4ead-b16a-740e2e7ef98d'
+};
